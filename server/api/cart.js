70f553ea9413a37208ba9ecac2a01d9c20e008b4
Chelsea Church <chelsea.ae.church@gmail.com>
2020-12-18 22:03:18 -0500

    refactor: updated cart get route to findAll()

--- server/api/cart.js
+++ server/api/cart.js
@@ -22,19 +22,32 @@ module.exports = router
 //   }
 // })
 
-//"VIEW CART" FOR LOGGED IN USERS OR GUESTS
+//Finds existing carts or creates new carts for logged in users and guests
 router.get('/', async (req, res, next) => {
   try {
     if (req.user) {
-      const userCart = await Order.findOrCreate({
+      const userCart = await Order.findAll({
         where: {userId: req.user.id, isComplete: false},
         include: [{model: Product}]
       })
-      res.json(userCart)
+      //Send back cart data or a message that the cart is empty
+      if (userCart[0].products.length === 0) {
+        res.send('Your Cart is Empty!')
+      } else {
+        res.json(userCart)
+      }
     } else {
-      const cart = new OrderItem(req.session.cart ? req.session.cart : {})
-      req.session.cart = cart
-      res.json(req.session.cart)
+      // const guestCart = await Order.findOrCreate({
+      //   where: {userId: req.session.cart.id, isComplete: false},
+      //   include: [{model: Product}]
+      // })
+      // console.log('REQ.SESSION >>>>> ', req.session)
+      // console.log('DATAVALUES >>>>> ', req.session.cart)
+      // res.json(guestCart)
+      const guestCart = new Order(req.session.order ? req.session.order : {})
+      req.session.order = guestCart
+      console.log('req.session.cart >>>', req.session.order.dataValues)
+      res.json(guestCart)
     }
   } catch (err) {
     next(err)
