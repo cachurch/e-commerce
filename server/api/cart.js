9c5059af24b0b6b361b7f2953845bbc752280b48
Chelsea Church <chelsea.ae.church@gmail.com>
2020-12-29 15:22:39 -0500

    refactor: POST cart route posts new orders and items for guest users

--- server/api/cart.js
+++ server/api/cart.js
@@ -19,17 +19,20 @@ router.get('/', async (req, res, next) => {
         res.json(userCart)
       }
     } else {
-      //Find an open cart for a guest
-      const guestCart = await Order.findOne({
-        where: {id: req.session.order.id, isComplete: false},
-        include: [{model: Product}]
+      // console.log(req.session.id, req.session)
+      // Find an open cart for a guest
+      const guestCart = await Order.findAll({
+        // where: {id: req.session.order.id, isComplete: false},
+        // include: [{model: Product}]
       })
       //Send back cart data or a message that the cart is empty
-      if (!guestCart || guestCart.products.length === 0) {
-        res.send('Your Cart is Empty!')
-      } else {
-        res.json(guestCart)
-      }
+      res.json(guestCart)
+
+      // if (!guestCart || guestCart.products.length === 0) {
+      //   res.send('Your Cart is Empty!')
+      // } else {
+      //   res.json(guestCart)
+      // }
     }
   } catch (err) {
     next(err)
@@ -38,9 +41,11 @@ router.get('/', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
   try {
-    console.log(req.body)
-    if (req.user) req.user.id = 1
-    else req.user = {id: 1}
+    // // For testing the user -----------------
+    // if (req.user) req.user.id = 1
+    // else req.user = {id: 1}
+    // -----------------------------------------
+
     //finds or creates open carts for users
     if (req.user) {
       const userCart = await Order.findOrCreate({
@@ -55,27 +60,85 @@ router.post('/', async (req, res, next) => {
 
       res.json({userCart, newItem})
     } else {
-      //Creates a cart for guests > method needs to be updated to find or create
-      const guestCart = await Order.findOrCreate({
-        where: {id: req.session.order.id, isComplete: false},
-        include: [{model: Product}]
-      })
-      //This sets the order id just created to the order id on the session
-      req.session.order.id = guestCart.dataValues.id
+      const hello = 'hi'
+      if (req.session.order) {
+        const guestCart = await Order.findOne({
+          where: {id: req.session.order.id, isComplete: false},
+          include: [{model: Product}]
+        })
+        console.log(
+          '>>>>>> POST REQ.SESSION >>>>>>> ',
+          req.session.id,
+          req.session
+        )
+        // console.log('guestCart >>>>>>>>>>>>>>> ', guestCart)
+        const newItem = await OrderItem.create({
+          productId: req.body.productId,
+          orderId: guestCart.id
+        })
 
-      //Add OrderItem create() method here
+        res.json({guestCart, newItem})
+      } else {
+        const guestCart = await Order.create({})
+        req.session.order = {}
+        req.session.order.id = guestCart.dataValues.id
 
-      res.json(guestCart)
+        const newItem = await OrderItem.create({
+          productId: req.body.productId,
+          orderId: guestCart.id
+        })
+        res.json({guestCart, newItem})
+      }
+
+      //-----------------------------------------------------------------------
+      // //Creates a cart for guests > method needs to be updated to find or create
+
+      // // req.session = {}
+
+      // const guestCart = await Order.findOrCreate({
+      //   where: {id: req.session.order.id, isComplete: false},
+      //   include: [{model: Product}]
+      // })
+
+      // const newItem = await OrderItem.create({
+      //   productId: req.body.productId,
+      //   orderId: guestCart[0].id
+      // })
+
+      // //This sets the order id just created to the order id on the session
+      // req.session.order.id = guestCart.dataValues.id
+
+      // //Add OrderItem create() method here
+
+      // res.json({guestCart, newItem})
+      //---------------------------------------------------------------------------------
     }
   } catch (error) {
     next(error)
   }
 })
 
-// //--------------------Test if this works in guest cart post -------------------
-// OrderItem.create({
-//   where: {
-//     productId: req.body.productId,
-//     orderId: req.session.order.id
-//   }})
-// //----------------------------------------------------------
+// //----------------------------------------------------------------------------
+
+//  // req.session = {}
+//  if (req.session.order) {
+//   const guestCart = await Order.findOne({
+//     where: {id: req.session.order.id, isComplete: false},
+//     include: [{model: Product}]
+//   })
+//   const newItem = await OrderItem.create({
+//   productId: req.body.productId,
+//   orderId: guestCart.id
+//   })
+//   res.json({guestCart, newItem})
+// } else {
+//   // req.session.order = {}
+//   const guestCart = await Order.create({})
+//   req.session.order.id = guestCart.dataValues.id
+
+//   const newItem = await OrderItem.create({
+//   productId: req.body.productId,
+//   orderId: guestCart.id
+//   })
+//   res.json({guestCart, newItem})
+// }
