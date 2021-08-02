feb8d7ebdca8f905e1a3adaee7a37026a75e6d13
Chelsea Church <chelsea.ae.church@gmail.com>
2021-08-02 16:02:29 -0400

    ssl update

--- server/db/db.js
+++ server/db/db.js
@@ -7,8 +7,9 @@ const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
     logging: false,
-    ssl: {
-      rejectUnauthorized: false
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: true
     }
   }
 )
