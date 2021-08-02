4d99254f48bbdb56907d81b8011049ab903b241c
Chelsea Church <chelsea.ae.church@gmail.com>
2021-08-02 15:58:12 -0400

    updated ssl settings

--- server/db/db.js
+++ server/db/db.js
@@ -6,8 +6,10 @@ const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
-    logging: false
-    //
+    logging: false,
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 )
 module.exports = db
